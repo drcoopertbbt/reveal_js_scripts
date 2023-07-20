0e2204a504fd6a9fe6cf2c191871265b7a7cea0d
dkypuros <davidkypuros@gmail.com>
2023-07-20 18:48:15 -0500

    Update flush-all.sh

--- flush-all.sh
+++ flush-all.sh
@@ -1,38 +1,24 @@
 #!/bin/bash
 
-# Script for setting up reveal.js in GitHub CodeSpaces
+REPO_URL="https://github.com/hakimel/reveal.js.git"
+TARGET_DIR="reveal.js"
 
-# Check the node and npm versions
 echo "Node and npm versions:"
-node --version
-npm --version
+node -v
+npm -v
 
-# Check if reveal.js repository exists
-if [ -d "reveal.js" ]; then
-    # If exists, pull latest changes
-    echo "Updating reveal.js repository..."
-    cd reveal.js
-    git pull
-else
-    # If not exists, clone reveal.js repository
-    echo "Cloning reveal.js repository..."
-    git clone https://github.com/hakimel/reveal.js.git
-    cd reveal.js
-fi
+echo "Removing existing reveal.js directory if it exists..."
+rm -rf $TARGET_DIR
 
-# Update host in gulpfile.js
+echo "Cloning reveal.js repository..."
+git clone $REPO_URL $TARGET_DIR
+
 echo "Updating host in gulpfile.js..."
-sed -i "s/host = 'localhost'/host = '0.0.0.0'/g" gulpfile.js
+sed -i 's/localhost/0.0.0.0/g' $TARGET_DIR/gulpfile.js
 
-# Remove node_modules and package-lock.json
-echo "Clearing npm environment..."
-rm -rf node_modules
-rm -f package-lock.json
-
-# Install reveal.js using npm
-echo "Rebuilding npm environment and installing reveal.js..."
+echo "Installing reveal.js dependencies..."
+cd $TARGET_DIR
 npm install
 
-# Start reveal.js
 echo "Starting reveal.js..."
 npm start
