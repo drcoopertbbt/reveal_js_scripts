710ae66d7b25ec145c0d6d080cebda4893349372
dkypuros <davidkypuros@gmail.com>
2023-07-20 18:53:55 -0500

    Create start-npm-only.sh

---
+++ start-npm-only.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Script for setting up reveal.js in GitHub CodeSpaces
+
+# Check the node and npm versions
+echo "Node and npm versions:"
+node --version
+npm --version
+
+# Start reveal.js
+echo "Starting reveal.js..."
+npm start
